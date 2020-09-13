75ba8c1b49ae6b4823c361db3832b4cb22066eac
Joo Hyun Kim <stevekim1109@gmail.com>
2020-09-13 00:43:20 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(strangeLoop) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      strangeLoop.push("I am 1 strange loop.")
+    } else {
+      strangeLoop.push(`I am ${i} strange loops.`)
+    }
+  }
+  return strangeLoop;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+  }
+  return "done";
+}
